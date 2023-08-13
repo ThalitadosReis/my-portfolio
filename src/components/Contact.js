e2b06f5ed938a4a616d2ea7182a910f9f35a681a
Thalita dos Reis <dosreistha@gmail.com>
2023-08-13 19:44:44 +0200

    modified email option

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,122 +1,20 @@
-import { useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import contactImg from "../assets/img/contact-img.png";
-
 export default function Contact() {
-  const formInitialDetails = {
-    fullName: "",
-    email: "",
-    phone: "",
-    message: "",
-  };
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState("Send");
-  const [status, setStatus] = useState({});
-
-  const onFormUpdate = (category, value) => {
-    setFormDetails({
-      ...formDetails,
-      [category]: value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setButtonText("Sending...");
-
-    try {
-      const response = await fetch("/.netlify/functions/sendEmail", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formDetails),
-      });
-
-      const result = await response.json();
-      if (result.code === 200) {
-        setStatus({ success: true, message: "Message sent successfully" });
-      } else {
-        setStatus({
-          success: false,
-          message: "Failed to send message",
-        });
-      }
-    } catch (error) {
-      setStatus({
-        success: false,
-        message: "Failed to send message",
-      });
-    } finally {
-      setButtonText("Send");
-      setFormDetails(formInitialDetails);
-    }
-  };
-
   return (
     <section className="contact" id="connect">
-      <Container>
-        <Row className="align-items-center">
-          <Col size={12} md={6}>
-            <img src={contactImg} alt="Contact Me" />
-          </Col>
-          <Col size={12} md={6}>
+      <div className="container">
+        <div className="align-items-center my-5">
+          <div className="text-center">
             <div>
               <h2>Get In Touch</h2>
-              <form onSubmit={handleSubmit}>
-                <Row>
-                  <Col size={12} sm={12} className="px-1">
-                    <input
-                      type="text"
-                      value={formDetails.fullName}
-                      placeholder="Full Name"
-                      onChange={(e) => onFormUpdate("fullName", e.target.value)}
-                    />
-                  </Col>
-                  <Col size={12} sm={6} className="px-1">
-                    <input
-                      type="email"
-                      value={formDetails.email}
-                      placeholder="Email Address"
-                      onChange={(e) => onFormUpdate("email", e.target.value)}
-                    />
-                  </Col>
-                  <Col size={12} sm={6} className="px-1">
-                    <input
-                      type="tel"
-                      value={formDetails.phone}
-                      placeholder="Phone No."
-                      onChange={(e) => onFormUpdate("phone", e.target.value)}
-                    />
-                  </Col>
-                  <Col size={12} sm={12} className="px-1">
-                    <textarea
-                      rows="6"
-                      value={formDetails.message}
-                      placeholder="Message"
-                      onChange={(e) => onFormUpdate("message", e.target.value)}
-                    ></textarea>
-                    <button type="submit">
-                      <span>{buttonText}</span>
-                    </button>
-                  </Col>
-                  {status.message && (
-                    <Col className="mt-3">
-                      <p
-                        className={
-                          status.success === false ? "danger" : "success"
-                        }
-                      >
-                        {status.message}
-                      </p>
-                    </Col>
-                  )}
-                </Row>
-              </form>
+              <p>
+                Have a question or want to discuss a project? Feel free to get
+                in touch.
+              </p>
+              <a href="mailto:dosreistha@gmail.com">Contact Me</a>
             </div>
-          </Col>
-        </Row>
-      </Container>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,17 @@
-import { Container, Row, Col } from "react-bootstrap";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <Container>
-        <Row className="align-items-center">
-          <Col size={12} sm={6}>
+      <div className="container">
+        <div className="align-items-center row">
+          <div className="col" size={12} sm={6}>
             <span>
               dos<b>Reis</b>
             </span>
-          </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
+          </div>
+          <div size={12} sm={6} className="text-center text-sm-end col">
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/thalitadosreis/">
                 <img src={navIcon1} alt="Icon" />
@@ -21,9 +20,9 @@ export default function Footer() {
                 <img src={navIcon2} alt="Icon" />
               </a>
             </div>
-          </Col>
-        </Row>
-      </Container>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
