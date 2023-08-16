5ce57fc5eaea2f94dcea74b842cc310d6d5b222e
Thalita dos Reis <dosreistha@gmail.com>
2023-08-16 18:52:18 +0200

    banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -23,22 +23,20 @@ export default function Banner() {
               front-end and back-end development, I can bring a unique
               perspective to any project.
             </p>
-            <button className="link">
-              <span className="link-text">Recent projects</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-arrow-down"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
-                />
-              </svg>
-            </button>
+            <ul className="buttons">
+              <li>
+                <a href="https://domain.ext/path">
+                  <span>Recent projects</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                  >
+                    <path d="M13 17.586V4h-2v13.586l-6.293-6.293-1.414 1.414L12 21.414l8.707-8.707-1.414-1.414L13 17.586z" />
+                  </svg>
+                </a>
+              </li>
+            </ul>
           </Container>
         </Col>
       </Row>
