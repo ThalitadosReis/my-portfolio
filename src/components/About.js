667bdb73dfe860073af24396d5a21ceace36729b
Thalita dos Reis <dosreistha@gmail.com>
2025-01-18 16:06:41 +0100

    update of about me section

--- src/components/About.js
+++ src/components/About.js
@@ -21,19 +21,37 @@ export default function About() {
           <div className="flex-1 text-center lg:text-left">
             <h1 className="uppercase text-[30px] lg:text-[45px] leading-[1] text-center lg:text-left lg:break-words lg:whitespace-pre-wrap">
               I use my {"\n"}background in {"\n"}
-              <span className="font-newyork font-normal">Hospitality </span>to build
-              <span className="font-newyork font-normal"> customer-focused </span>
+              <span className="font-newyork font-normal">Hospitality </span>to
+              build
+              <span className="font-newyork font-normal">customer-focused</span>
               solutions.
             </h1>
           </div>
           <div className="flex-1 text-base text-justify">
-            <p>
-              I am a full-stack web developer with a passion for creating
-              interactive and responsive web applications. I have experience
-              working with JavaScript, React, Node.js, Express, MongoDB, HTML,
-              CSS, and Git. I am a quick learner and always looking to expand my
-              knowledge and skill set. I thrive in collaborative environments
-              and am excited to work with others to create amazing applications.
+            <p className="indent-8 mb-3">
+              Hi there! I'm Thalita, a <span className="underline">junior</span> full-stack developer based in Zürich with
+              a passion for learning and innovation. Beyond web development, I’m
+              deeply engaged with nature and adventure—whether I’m snowboarding
+              through the Swiss Alps in winter or enjoying lakeside sunshine in
+              summer. I’m also a culinary enthusiast, constantly experimenting
+              with international recipes, a creative practice that parallels my
+              approach to coding: blending curiosity with structure to achieve
+              something new.
+            </p>
+            <p className="indent-8 mb-3">
+              Before transitioning into tech, I worked in the hospitality
+              industry, where I gained skills in adaptability, teamwork, and
+              proactive problem-solving. These experiences taught me how to
+              connect with people and respond effectively to their needs,
+              qualities that I now bring to every team and project.
+            </p>
+            <p className="indent-8">
+              Looking ahead, I’m eager to deepen my expertise in full-stack
+              development and collaborate with teams that prioritize creativity
+              and growth. My goal is to contribute to meaningful projects,
+              continuously improving my technical skills while making a positive
+              impact in the tech industry. Let’s connect and explore how we can
+              bring fresh ideas to life together!
             </p>
           </div>
         </div>
